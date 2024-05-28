0bb4e0dd32738fc4f73f087ba24c73c61dcc2d62
ArnauMP <Inferno6486@gmail.com>
2024-05-28 18:55:58 +0200

    Inicio de creacion del formulario para ser vip

--- app/src/main/java/com/example/arnau_munoz_dam2/Home.java
+++ app/src/main/java/com/example/arnau_munoz_dam2/Home.java
@@ -2,7 +2,11 @@ package com.example.arnau_munoz_dam2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button vipButton = findViewById(R.id.vipButton);
+
+        vipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Vip.class);
+                startActivity(intent);
+            }
+        });
     }
 }
